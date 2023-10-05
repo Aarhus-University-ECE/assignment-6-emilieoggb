8f219b6323598ec28c008538548742a463a3e6f2
Emilie <emilie.b.holmberg@hotmail.dk>
2023-10-05 12:19:31 +0200

    task 2

--- max.c
+++ max.c
@@ -5,13 +5,27 @@
 int max(int *numbers, int size) {
   // Excercise 2
   // Implement your code below...
+  assert(size > 0);
 
-  return -1;
+  int max = numbers[0];
+  
+  for(int i = 1; i < size; i++){
+    if(numbers[i] > max){
+      max = numbers[i];
+    }
+  }
+
+  return max;
 }
 
 #ifndef TEST
 
 int main() {
+  
+  //test
+  int numbers[] = {5, 10, 20, 6, 10, -1, 9};
+  int size = 7;
+  printf("%d\n", max(numbers, size));
 
 	return 0;
 }
